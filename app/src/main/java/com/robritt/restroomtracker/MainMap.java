047193095d67743634239354bbf51f40152146ed
xbritbx <xbritbx@gmail.com>
2019-06-16 22:29:13 -0400

    Added some testing button functionality and Google Maps Screen

--- app/src/main/java/com/robritt/restroomtracker/MainMap.java
+++ app/src/main/java/com/robritt/restroomtracker/MainMap.java
@@ -1,7 +1,9 @@
 package com.robritt.restroomtracker;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,6 +26,36 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
+    public void openListScreen(View view){
+        Intent intent = new Intent(this, ListOfRestroomsScreen.class);
+        startActivity(intent);
+    }
+
+
+    public void openAddScreen(View view){
+        Intent intent = new Intent(this, AddARestroomScreen.class);
+        startActivity(intent);
+    }
+
+    public void openFavScreen(View view){
+        Intent intent = new Intent(this, FavoriteRestroomsScreen.class);
+        startActivity(intent);
+    }
+
+    public void openSettingsScreen(View view){
+        Intent intent = new Intent(this, SettingsScreen.class);
+        startActivity(intent);
+    }
+
+    public void openARScreen(View view){
+        Intent intent = new Intent(this, ArScreen.class);
+        startActivity(intent);
+    }
+
+    public void openFilterScreen(View view){
+        Intent intent = new Intent(this, FilterScreen.class);
+        startActivity(intent);
+    }
 
     /**
      * Manipulates the map once available.
@@ -45,4 +77,7 @@ public class MainMap extends FragmentActivity implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+
 }
