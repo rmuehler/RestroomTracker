d330e450fbff110454918d4b605dcb3991ce87c6
Robert Muehler <robertmuehler@gmail.com>
2019-07-07 17:25:04 -0400

    added new tags and stuff to database

--- app/src/main/java/com/robritt/restroomtracker/ArScreen.java
+++ app/src/main/java/com/robritt/restroomtracker/ArScreen.java
@@ -3,18 +3,21 @@ package com.robritt.restroomtracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.ux.ArFragment;
+
 import java.util.concurrent.CompletableFuture;
 
 import uk.co.appoly.arcorelocation.LocationScene;
 
-public class ArScreen extends AppCompatActivity {
+public class ArScreen extends AppCompatActivity{
 
     private LocationScene locationScene;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ar_screen);
+//        setContentView(R.layout.activity_ar_screen);
 
 //        CompletableFuture<ModelRenderable>/ andy = Model
 //        CompletableFuture<ModelRe
