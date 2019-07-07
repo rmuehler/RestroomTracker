06bea7f3f4fcf1fee6fd9e1a6305b9fc36cc67cc
Robert Muehler <robertmuehler@gmail.com>
2019-07-07 02:30:59 -0400

    added database stuff and image uploading for add screen NOTE: camera images are still not deleted so they can eat phone space.

--- app/src/main/java/com/robritt/restroomtracker/AddARestroomScreen.java
+++ app/src/main/java/com/robritt/restroomtracker/AddARestroomScreen.java
@@ -2,6 +2,7 @@ package com.robritt.restroomtracker;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,20 +10,27 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -37,18 +45,28 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.ServerTimestamp;
 import com.google.firebase.firestore.model.value.ServerTimestampValue;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.firestore.v1.DocumentTransform;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class AddARestroomScreen extends AppCompatActivity  {
 
     FirebaseFirestore db;
     FirebaseAuth mAuth;
+    FirebaseStorage mStorage;
+    StorageReference storageReference;
 
     private Button btnChoose;
     private Button btnTake;
@@ -58,6 +76,20 @@ public class AddARestroomScreen extends AppCompatActivity  {
     private final int TAKE_IMAGE_REQUEST = 52;
     private static final int PERMISSION_REQUEST_CODE = 200;
 
+    EditText eName;
+    EditText eOpenTime;
+    EditText eCloseTime;
+    CheckBox cTwentyFour;
+    RatingBar rCleanliness;
+    RatingBar rPrivacy;
+    Switch sBaby;
+    Switch sHandicap;
+    Switch sKey ;
+    EditText eFloor;
+    RadioGroup rgAutomatic ;
+    RadioGroup rgBuilding ;
+    EditText eDirections ;
+    Spinner spStalls;
 
 
 
@@ -66,6 +98,8 @@ public class AddARestroomScreen extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
+        mStorage = FirebaseStorage.getInstance();
+        storageReference = mStorage.getReference();
         setContentView(R.layout.activity_add_arestroom_screen);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
@@ -73,6 +107,22 @@ public class AddARestroomScreen extends AppCompatActivity  {
 
         final EditText chooseOpenTime = findViewById(R.id.openTime);
         final EditText chooseCloseTime = findViewById(R.id.closeTime);
+        eName = findViewById(R.id.etRestroomName); //restroom name
+        eOpenTime = findViewById(R.id.openTime);
+        eCloseTime = findViewById(R.id.closeTime);
+        cTwentyFour = findViewById(R.id.checkBox); //open 24 hours
+        rCleanliness = findViewById(R.id.starsCleanliness);
+        rPrivacy = findViewById(R.id.startsPrivacy);
+        sBaby = findViewById(R.id.babySwitch);
+        sHandicap = findViewById(R.id.handicapSwitch);
+        sKey = findViewById(R.id.keySwitch);
+        eFloor = findViewById(R.id.eFloor);
+        rgAutomatic = findViewById(R.id.radioGroup); //automatic toilets
+        rgBuilding = findViewById(R.id.radioGroup2); //inside building?
+        eDirections = findViewById(R.id.buildingDescription);
+        spStalls = findViewById(R.id.numStalls);
+
+        filePath = null;
 
         chooseOpenTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -231,7 +281,26 @@ public class AddARestroomScreen extends AppCompatActivity  {
     }
     private void takeImage() {
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePicture, TAKE_IMAGE_REQUEST);//zero can be replaced with any action code
+
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                filePath = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, filePath);
+                startActivityForResult(takePicture, TAKE_IMAGE_REQUEST);//zero can be replaced with any action code
+            }
+        }
+
+
     }
 
     @Override
@@ -239,16 +308,17 @@ public class AddARestroomScreen extends AppCompatActivity  {
         super.onActivityResult(requestCode, resultCode, data);
         switch(requestCode) {
             case 71:
-                if(resultCode == RESULT_OK){
+                if(resultCode == RESULT_OK){ //pick image
                     Uri selectedImage = data.getData();
+                    filePath = data.getData();
                     imageView.setImageURI(selectedImage);
                 }
 
                 break;
             case 52:
-                if(resultCode == RESULT_OK){
-                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-                    imageView.setImageBitmap(photo);
+                if(resultCode == RESULT_OK){ //camera
+//                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imageView.setImageURI(filePath);
                 }
                 break;
         }
@@ -262,19 +332,87 @@ public class AddARestroomScreen extends AppCompatActivity  {
     }
 
     public void addRestroom(View view){
+        //input validation for required stuff
+
+        if (eName.getText().toString().length() == 0){ //name requiremenet
+            eName.setError("Name required");
+            return;
+        }
+        if (eOpenTime.getText().toString().length() == 0 && cTwentyFour.isChecked() == false){ //time requiremenet
+            eOpenTime.setError("Time required");
+            return;
+        }
+        if (eCloseTime.getText().toString().length() == 0 && cTwentyFour.isChecked() == false){ //time requiremenet
+            eCloseTime.setError("Time required");
+            return;
+        }
+        if (eFloor.getText().toString().length() == 0){ //floor requiremenet
+            eFloor.setError("Floor required");
+            return;
+        }
+
+
+
+
+
         final double latitude = getIntent().getDoubleExtra("latitude",0);
         final double longitude = getIntent().getDoubleExtra("longitude", 0);
         GeoPoint geopoint = new GeoPoint(latitude,longitude);
         final Map<String,Object> newRestroom = new HashMap<>();
-        newRestroom.put("location", geopoint);                  //add fields that go into each restroom here
+        final Map<String, Object> cleanlinessRating = new HashMap<>();
+        final Map<String, Object> privacyRating = new HashMap<>();
+
+        //image upload
+
+
+
+
+        newRestroom.put("image", uploadImage().getPath());
+        //required stuff
+        newRestroom.put("name", eName.getText().toString());
         newRestroom.put("open", true);
-        newRestroom.put("createdby", mAuth.getCurrentUser().getUid());
-        newRestroom.put("created", FieldValue.serverTimestamp());
+
+        if(cTwentyFour.isChecked() == false) {
+            newRestroom.put("opens", eOpenTime.getText().toString());
+            newRestroom.put("closes", eCloseTime.getText().toString());
+        }
+        else{
+            newRestroom.put("opens", "24 hours");
+            newRestroom.put("closes", "24 hours");
+        }
+
+        newRestroom.put("floor", eFloor.getText().toString());
+
+
+        //DOES NOT NEED VALIDATION
+        newRestroom.put("babychanging", sBaby.isChecked());
+        newRestroom.put("handicapped", sHandicap.isChecked());
+        newRestroom.put("keyrequired", sKey.isChecked());
+
+        cleanlinessRating.put(mAuth.getCurrentUser().getUid(), rCleanliness.getRating()); //we need to store this in maps to the UID is linked to rating
+        newRestroom.put("cleanliness", cleanlinessRating);
+
+        privacyRating.put(mAuth.getCurrentUser().getUid(), rPrivacy.getRating());
+        newRestroom.put("privacy", privacyRating);
+
+        //optional stuff
+        newRestroom.put("automatictoilets", rgAutomatic.getCheckedRadioButtonId());
+        newRestroom.put("insidebuilding", rgBuilding.getCheckedRadioButtonId());
+        newRestroom.put("directions", eDirections.getText().toString());
+        newRestroom.put("stalls", spStalls.getSelectedItem().toString());
+
+        //automatic stuff
+        newRestroom.put("createdby", mAuth.getCurrentUser().getUid()); //UID of creator
+        newRestroom.put("created", FieldValue.serverTimestamp()); //time created
+        newRestroom.put("location", geopoint);
         db.collection("restrooms").add(newRestroom).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
             public void onSuccess(DocumentReference documentReference) {
                 Toast.makeText(AddARestroomScreen.this, "Added new restroom", Toast.LENGTH_SHORT).show();
                 setResult(RESULT_OK);
+//                File file = new File(filePath.getPath());// TODO make this delete local photo file ONLY IF CAMERA WAS USED TO TAKE PHOTO
+//                file.delete();
+                filePath = null;
                 finish();
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -282,9 +420,65 @@ public class AddARestroomScreen extends AppCompatActivity  {
             public void onFailure(@NonNull Exception e) {
                 setResult(RESULT_CANCELED);
                 Toast.makeText(AddARestroomScreen.this, "Add unsuccessful", Toast.LENGTH_SHORT).show();
-                finish();
+//                finish();
             }
         });
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        filePath = Uri.fromFile(image);
+        return image;
+    }
+
+    public StorageReference uploadImage(){
+            if(filePath != null) {
+//            Toast.makeText(this, "Uploading image", Toast.LENGTH_SHORT).show();
+                final StorageReference ref = storageReference.child("restroom-images/" + UUID.randomUUID().toString());
+                ref.putFile(filePath)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            Toast.makeText(AddARestroomScreen.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(AddARestroomScreen.this, "Added to " + ref.getPath(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AddARestroomScreen.this, "Failed image upload" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                //TODO this causes issues when image upload fails as it still returns the correct ref
+
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+//                                    .getTotalByteCount()); //TODO maybe use later?
+                            }
+                        });
+                return ref;
+            }
+
+            else{
+                return storageReference.child("restroom-images/default"); //default file when image upload fails
+            }
+
+
+
     }
 
 }
