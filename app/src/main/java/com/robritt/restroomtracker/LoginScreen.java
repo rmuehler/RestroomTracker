1c762fb47e81c1f341fc4c340a7abdab0c5e49a5
Robert Muehler <robertmuehler@gmail.com>
2019-07-08 00:14:24 -0400

    quick login button

--- app/src/main/java/com/robritt/restroomtracker/LoginScreen.java
+++ app/src/main/java/com/robritt/restroomtracker/LoginScreen.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +24,7 @@ public class LoginScreen extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     EditText mEmailField, mPasswordField;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,35 @@ public class LoginScreen extends AppCompatActivity {
 
         mEmailField = (EditText) findViewById(R.id.username_login_field);
         mPasswordField = (EditText) findViewById(R.id.password_login_field);
+        submit = findViewById(R.id.login_button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
+
+
+        mPasswordField.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    submit.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
-    public void openMap(View view) { //Change later for validating login, this is just for testing purposes
+    public void openMap() { //Change later for validating login, this is just for testing purposes
         Intent intent = new Intent(this, MainMap.class);
         startActivity(intent);
     }
 
-    public void startSignIn(final View view) {
+    private void startSignIn() {
 
         String email = mEmailField.getText().toString();
         String password = mPasswordField.getText().toString();
@@ -52,7 +77,7 @@ public class LoginScreen extends AppCompatActivity {
                     if (task.isSuccessful()) { //if signup successful
                         Log.d("ACCOUNT", "loginUserWithEmail:success");
                         FirebaseUser user = mAuth.getCurrentUser();
-                        updateUI(view,user);
+                        updateUI(user);
 
 
                     } else { //if signup fails
@@ -82,9 +107,9 @@ public class LoginScreen extends AppCompatActivity {
         });
     }
 
-    private void updateUI(View view,FirebaseUser user) {//checks to see if the user is logged in, if he is take him to map
+    private void updateUI(FirebaseUser user) {//checks to see if the user is logged in, if he is take him to map
         if (user != null) { //user is signed in
-            openMap(view);
+            openMap();
         } else {
             //user has not signed in to the app, no redirect
         }
