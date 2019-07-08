28704d53b0bb4379702c922448487403900cc070
xbritbx <xbritbx@gmail.com>
2019-07-08 00:50:05 -0400

    Changed View Restroom Screen

--- app/src/main/java/com/robritt/restroomtracker/ListOfRestroomsScreen.java
+++ app/src/main/java/com/robritt/restroomtracker/ListOfRestroomsScreen.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
@@ -30,11 +35,23 @@ public class ListOfRestroomsScreen extends AppCompatActivity {
 
     FirebaseFirestore db;
     double latitude, longitude;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    Location currentLocation;
+    /*
     Location currentLocation;
     ArrayList<String> listItems;
     ArrayAdapter<String> adapter;
+
+    */
     Map<Long,String> listToID = new HashMap<>();
     TreeMap<Float, String> distanceID = new TreeMap<>();
+
+
+    ArrayList<String> myDataset;
 
 
     @Override
@@ -45,19 +62,42 @@ public class ListOfRestroomsScreen extends AppCompatActivity {
         Intent intent = getIntent();
         latitude = getIntent().getDoubleExtra("latitude",0);
         longitude = getIntent().getDoubleExtra("longitude", 0);
+
+        myDataset = new ArrayList<String>();
+        myDataset.add("Hello");
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
         currentLocation = new Location("Current Location");
         currentLocation.setLongitude(longitude);
         currentLocation.setLatitude(latitude);
+
+
+
+
 //        HashMap<Marker, String> markerToID = (HashMap<Marker, String>) intent.getSerializableExtra("map"); //gets the marker/ ID referance from mainmap
+
+        /*
         //List stuff
         ListView mList = (ListView) findViewById(R.id.list_nearby);
         listItems = new ArrayList<String>();
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
         mList.setAdapter(adapter);
 
-
         updateRestroomsList(25); //TODO allow user to choose how many to display
-
 
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -76,8 +116,10 @@ public class ListOfRestroomsScreen extends AppCompatActivity {
             }
         });
 
-    } //end oncreate
+        */
 
+    } //end oncreate
+/*
     private void updateRestroomsList(final int numberToDisplay) { //get all restrooms in database with the value "open"
 
         db.collection("restrooms")
@@ -125,24 +167,15 @@ public class ListOfRestroomsScreen extends AppCompatActivity {
                             listID++;
                         }
 
-
-
-
-
-
-
-
-
-
-
                     }
                 });
 
     } //end restroomlocatiosn
 
 
-
+*/
 
 } //end class
+
 
 
